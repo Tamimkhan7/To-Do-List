78dd2ba56ef9464b911b0f3288330790429eba6a
Tamim khan <mdtamimkhan842@gmail.com>
2025-02-15 20:36:08 +0600

    update to do apps

--- app.js
+++ app.js
@@ -3,42 +3,65 @@ let addbtn = document.querySelector('#addbtn');
 let todolist = document.querySelector('.todolist');
 
 let gettodos = JSON.parse(localStorage.getItem('todo-list')) || [];
+let localInput = null;
 
 function addtodo() {
-    let inputtext = inputbox.value.trim(); //why use trim -- Removes any leading or trailing whitespace (spaces, newlines, and tabs) from the input value.
+    let inputtext = inputbox.value.trim();
+    if (inputtext.length < 1) {
+        alert('Please write a to-do');
+        return;
+    }
 
-    gettodos.push(inputtext);
-    localStorage.setItem('todo-list', JSON.stringify(gettodos)); //localstroage always key pair value
-    inputbox.value = ''; //inputbox create empty string
+    if (addbtn.value == 'Edit') {
+        let todo = localInput.target.previousElementSibling.innerText;
+        let todoindex = gettodos.indexOf(todo);
+        if (todoindex !== -1) {
+            gettodos[todoindex] = inputtext;
+        }
+    } else {
+        if (gettodos.includes(inputtext)) {
+            alert('This to-do already exists!');
+            return;
+        }
+        gettodos.push(inputtext);
+    }
+
+    localStorage.setItem('todo-list', JSON.stringify(gettodos));
+    addbtn.value = 'Add';
+    inputbox.value = '';
+    showLocalTodo();
 }
+
 function showLocalTodo() {
     todolist.innerHTML = gettodos.map(item =>
-        `  <li>
+        `<li>
           <p>${item}</p>
           <button class="btn editbtn">Edit</button>
           <button class="btn deletebtn">Delete</button>
-        </li>
-        `
+        </li>`
     ).join('');
-    inputbox.value = '';
 }
+
 function updateTodo(e) {
     let value = e.target.innerText;
     if (value == 'Delete') {
         let todotext = e.target.parentElement.children[0].innerText;
         let todoindex = gettodos.indexOf(todotext);
-        gettodos.splice(todoindex, 1);
-        localStorage.setItem('todo-list', JSON.stringify(gettodos));
-        showLocalTodo();
+        if (todoindex !== -1) {
+            gettodos.splice(todoindex, 1);
+            localStorage.setItem('todo-list', JSON.stringify(gettodos));
+            showLocalTodo();
+        }
     }
 
     if (value == 'Edit') {
         inputbox.value = e.target.previousElementSibling.innerText;
         addbtn.value = 'Edit';
-        inputbox.focus();//Puts the cursor inside the input box, making it ready for editing.
+        inputbox.focus();
         localInput = e;
     }
 }
+
 todolist.addEventListener('click', updateTodo);
 window.addEventListener('load', showLocalTodo);
 addbtn.addEventListener('click', addtodo);
